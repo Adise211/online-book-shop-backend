fecbec72feb86fdb5fb773519dc119b861fcd7e1
Adise <adise@wyze.co.il>
2025-05-04 18:51:27 -0400

    import at test prisma migrate

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
 import express from "express";
-// import todoRoutes from "./routes/todos.js";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 // app.use(todoRoutes);
@@ -12,7 +13,35 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req.method);
+
   res.json({ message: "Halo Word!" });
 });
 
 app.listen(3030);
+
+// console.log("Prisma:", prisma);
+async function main() {
+  // const allUsers = await prisma["student-test"].findMany();
+  // Run inside `async` function
+  const user = await prisma.user.create({
+    data: {
+      name: "Max",
+      email: "max@prisma.io",
+      posts: {
+        create: { title: "Join us for Prisma Day 2020" },
+      },
+    },
+  });
+  // console.log("prisma?", prisma);
+}
+
+main()
+  .then(async () => {
+    // await prisma.$disconnect();
+    console.log("User was created!");
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
