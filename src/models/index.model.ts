e3608e5aa173d7493e9b60151ce4bfdfd7e71e09
Adise <adise@wyze.co.il>
2025-05-07 15:19:17 -0400

    added link to CRUD quries with Prisma

--- src/models/index.model.ts
+++ src/models/index.model.ts
@@ -2,4 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// Prisma CRUD queries - https://www.prisma.io/docs/orm/prisma-client/queries/crud
+
 export default prisma;
