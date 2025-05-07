c69446ad957068662964658790d0251c7276ce72
Adise <adise@wyze.co.il>
2025-05-06 21:12:05 -0400

    replaced google books api requests to dervices folder

--- src/controllers/books.ts
+++ src/controllers/books.ts
@@ -1,52 +1,15 @@
 import { Request, Response } from "express";
-import { ResponseToClient } from "../../types.js";
+import { ResponseToClient, Book } from "../../types.js";
 import { bookCategories } from "../utils/consts.js";
 import { PARAMETER_IS_REQUIRED } from "../utils/errorMessages.js";
 import { isCategorieExist } from "../utils/utilFunc.js";
+import { googleBooksAPIRequest } from "../service/index.js";
 
 // get best seller books (for home page) - ✅
 // get book categories - ✅
 // get a book by a categorie - ✅
 // TODO: add a book to favorites
 // TODO: remove a book from favorites
-
-const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
-// performence tips: limited amount of results, return spesific fields
-// more info: https://developers.google.com/books/docs/v1/performance
-const GOOGLE_BOOKS_FILTERED_FIELDS =
-  "kind, totalItems, items(id, volumeInfo(title, subtitle, authors, description, publishedDate, pageCount, categories, imageLinks, language ), saleInfo)";
-
-async function googleBooksAPIRequest(query: string): Promise<ResponseToClient> {
-  const response = await fetch(
-    `${GOOGLE_BOOKS_API}?q=${query}&fields=${GOOGLE_BOOKS_FILTERED_FIELDS}`
-  );
-
-  let books = await response.json();
-  let result: ResponseToClient;
-
-  if (response.ok) {
-    result = {
-      Result: {
-        ResultCode: 1,
-        ResultMessage: "",
-        IsError: false,
-        Source: "external",
-      },
-      Data: books,
-    };
-  } else {
-    result = {
-      Result: {
-        ResultCode: -1,
-        ResultMessage: books.error?.message,
-        IsError: true,
-        Source: "external",
-      },
-      Data: [],
-    };
-  }
-  return result;
-}
 
 export async function getHomePageBooks(req: Request, res: Response) {
   const result = await googleBooksAPIRequest("bestseller&orderBy=newest");
@@ -91,4 +54,8 @@ export async function getBooksByCategorie(req: Request, res: Response) {
     };
   }
   res.send(result);
+}
+
+export async function addBookToFavorites(book: Book) {
+  // Add to favorites table
 }
---
+++ src/service/index.ts
@@ -0,0 +1,41 @@
+import { ResponseToClient } from "../../types.js";
+
+const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
+// performence tips: limited amount of results, return spesific fields
+// more info: https://developers.google.com/books/docs/v1/performance
+const GOOGLE_BOOKS_FILTERED_FIELDS =
+  "kind, totalItems, items(id, volumeInfo(title, subtitle, authors, description, publishedDate, pageCount, categories, imageLinks, language ), saleInfo)";
+
+export async function googleBooksAPIRequest(
+  query: string
+): Promise<ResponseToClient> {
+  const response = await fetch(
+    `${GOOGLE_BOOKS_API}?q=${query}&fields=${GOOGLE_BOOKS_FILTERED_FIELDS}`
+  );
+
+  let books = await response.json();
+  let result: ResponseToClient;
+
+  if (response.ok) {
+    result = {
+      Result: {
+        ResultCode: 1,
+        ResultMessage: "",
+        IsError: false,
+        Source: "external",
+      },
+      Data: books,
+    };
+  } else {
+    result = {
+      Result: {
+        ResultCode: -1,
+        ResultMessage: books.error?.message,
+        IsError: true,
+        Source: "external",
+      },
+      Data: [],
+    };
+  }
+  return result;
+}
