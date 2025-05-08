1697686dd77b680ecac08e4b83f7c784a4ab4dbe
Adise <adise@wyze.co.il>
2025-05-08 18:29:47 -0400

    removed unnecessary file

--- src/models/index.model.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-// Prisma CRUD queries - https://www.prisma.io/docs/orm/prisma-client/queries/crud
-
-export default prisma;
