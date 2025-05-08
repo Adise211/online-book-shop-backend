ae10c7d9abc16a9611aac0d941ea54aaeaa96b52
Adise <adise@wyze.co.il>
2025-05-08 00:13:51 -0400

    jwt - verify token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,2 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/auth.utils.js";
 // TODO: authenticate user (email & password)
 // TODO: authorization (token)
+
+export function authenticateToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader?.split(" ")[1]; // Bearer <token>
+
+    if (!token) {
+      res.status(401).json({ message: "No token provided" });
+    } else {
+      const user = verifyToken(token);
+      (req as any).user = user; // attach user to request
+      next();
+    }
+  } catch (err) {
+    res.status(403).json({ message: "Invalid or expired token" });
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import {
   getBookCategoriesList,
   getBooksByCategorie,
 } from "../controllers/books.js";
+import { authenticateToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
@@ -16,5 +17,8 @@ router.post("/login", loginUser);
 router.get("/books", getHomePageBooks);
 router.get("/categories", getBookCategoriesList);
 router.get("/books/:categorie", getBooksByCategorie);
+router.get("/protected", authenticateToken, (req, res) => {
+  res.json({ message: "Success!", user: (req as any).user });
+});
 
 export default router;
