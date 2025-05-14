b5c3773d8503b77ac6978a9a966a633aa7b10b19
Adise <adise@wyze.co.il>
2025-05-14 12:24:23 -0400

    renamed model 'user' to 'users' in prisma.schema

---
+++ prisma/migrations/20250514160823_map_user_table_in_db/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Favorites" DROP CONSTRAINT "Favorites_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Reviews" DROP CONSTRAINT "Reviews_userId_fkey";
+
+-- DropTable
+DROP TABLE "Users";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL DEFAULT '',
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Favorites" ADD CONSTRAINT "Favorites_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reviews" ADD CONSTRAINT "Reviews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
