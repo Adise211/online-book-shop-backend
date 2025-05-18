a287508873d674195285fca7d8f1ccd723efeda1
Adise <adise@wyze.co.il>
2025-05-18 12:42:05 -0400

    installed cookie-parser

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import router from "./src/routes/index.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { appErrorHandler } from "./src/middlewares/errorHandlers.js";
 
 const app = express();
 dotenv.config();
 
 app.use(express.json()); // Enable parsing of JSON request bodies from raw stream
+app.use(cookieParser());
 
 app.use(router);
 app.use(appErrorHandler);
