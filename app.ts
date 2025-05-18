f55049941a81fccb280604c7307ec369e2ec29da
Adise <adise@wyze.co.il>
2025-05-18 15:40:15 -0400

    added cors config as middleware

--- app.ts
+++ app.ts
@@ -2,13 +2,20 @@ import express from "express";
 import router from "./src/routes/index.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { appErrorHandler } from "./src/middlewares/errorHandlers.js";
 
 const app = express();
 dotenv.config();
 
+const corsConfig = {
+  origin: process.env.FRONTEND_ORIGIN_DEV,
+  credentials: true, // Allow cookies (important)
+};
+
 app.use(express.json()); // Enable parsing of JSON request bodies from raw stream
 app.use(cookieParser());
+app.use(cors(corsConfig));
 
 app.use(router);
 app.use(appErrorHandler);
