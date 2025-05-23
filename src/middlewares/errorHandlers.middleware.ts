365f4dc1fff0a2ad30d766fc27312e4e7c8bd732
Adise <adise@wyze.co.il>
2025-05-23 11:55:44 -0400

    refactor fields validation middleware

--- src/middlewares/errorHandlers.middleware.ts
+++ src/middlewares/errorHandlers.middleware.ts
@@ -8,21 +8,25 @@ export function validateFields(
   requiredFields: string[]
 ) {
   return (req: Request, res: Response, next: NextFunction) => {
+    let message: AppError["message"] = "";
+    let statusCode: AppError["statusCode"] = 500;
+    let missingFields: string[] = [];
+
     if (!source || requiredFields.length <= 0) {
       // params to this function are missing
-      res.status(500).json({ message: "Internal server error" });
-      throw new Error(
-        "Error Handler 'validateFields': params are missing ('source: string' || 'requiredFields: string[]'"
+      console.error(
+        "Error Handler 'validateFields': params are missing ('source: string' || 'requiredFields: string[]')"
       );
+      message = "Internal server error";
+      statusCode = Codes.Server.General;
     } else {
       if (!req[source]) {
         // req.body || req.params || req.query was not found!
         console.error(
           `Error Handler 'validateFields': the '${source}' in request was not found`
         );
-        res
-          .status(400)
-          .json({ message: `'${source}' in request was not found` });
+        message = `'${source}' in request was not found`;
+        statusCode = Codes.Client.Bad_Request;
       } else if (
         req[source] &&
         requiredFields.length > 2 &&
@@ -32,12 +36,9 @@ export function validateFields(
         console.error(
           `Error Handler 'validateFields': fields are not wrapped with 'data' object`
         );
-        res
-          .status(400)
-          .json({ message: `fields are not wrapped with 'data' object` });
+        message = `fields are not wrapped with 'data' object`;
+        statusCode = Codes.Client.Bad_Request;
       } else {
-        let missingFields: string[] = [];
-
         // Check if required fields exist in the request.source (body, params or query)
         requiredFields.forEach((field) => {
           if (!req[source][field] && !req[source].data[field]) {
@@ -47,14 +48,13 @@ export function validateFields(
 
         // If fields are missing - send error message
         if (missingFields.length > 0) {
-          res
-            .status(400)
-            .json({ message: `Missing fields: (${missingFields.toString()})` });
+          message = `Missing fields: (${missingFields.toString()})`;
+          statusCode = Codes.Client.Bad_Request;
         }
       }
     }
 
-    next();
+    next(new AppError(message, statusCode, "system"));
   };
 }
 
