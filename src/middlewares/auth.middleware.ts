4e75f280606ad9839aceb1a1dbd73a0fb31bb75b
Adise <adise@wyze.co.il>
2025-05-23 14:32:50 -0400

    refactor auth token middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { verifyAccessToken } from "../utils/auth.utils.js";
+import { AppError, Codes } from "../utils/errors.utils.js";
+import { ResultSource } from "../types.js";
 // authenticate user (email & password) - ✅
 // authorization (token) - ✅
+
+let resultSource: ResultSource = "system";
 
 export function authenticateToken(
   req: Request,
@@ -9,17 +13,31 @@ export function authenticateToken(
   next: NextFunction
 ) {
   try {
+    // default error
+
     const authHeader = req.headers["authorization"];
     const token = authHeader?.split(" ")[1]; // Bearer <token>
 
     if (!token) {
-      res.status(401).json({ message: "No token provided" });
+      next(
+        new AppError(
+          "No token provided",
+          Codes.Client.Unauthorized,
+          resultSource
+        )
+      );
     } else {
       const user = verifyAccessToken(token);
       (req as any).user = user; // attach user to request
       next();
     }
   } catch (err) {
-    res.status(403).json({ message: "Invalid or expired token" });
+    next(
+      new AppError(
+        "Invalid or expired token",
+        Codes.Client.Forbidden,
+        resultSource
+      )
+    );
   }
 }
